620d61b903e8c930eb8e5dfcdc9ab646214de942
asteph <alissa.stephens1@gmail.com>
2015-07-23 13:38:56 -0500

    added additional functions, and kept canvas from dissappearing

--- classes.py
+++ classes.py
@@ -22,11 +22,25 @@ class Mammals(Animals):
 class Giraffes(Mammals):
 	def eat_leaves_from_trees(self):
 		print('eating leaves')
+	def left_foot_forward(self):
+		print('left foot forward')
+	def left_foot_back(self):
+		print('left foot back')
+	def right_foot_forward(self):
+		print('right foot forward')
+	def right_foot_back(self):
+		print('right foot back')
+	def dance(self):
+		self.left_foot_forward()
+		self.left_foot_back()
+		self.right_foot_forward()
+		self.right_foot_back()
 #creating objects in a class
 reginald = Giraffes()
 harold = Giraffes()
 #calling functions on objects
 reginald.move()
+reginald.dance()
 harold.eat_leaves_from_trees()
 
 #working with the turtle module
@@ -34,12 +48,30 @@ import turtle
 #creating turtle objects of the Pen class
 avery = turtle.Pen()
 kate = turtle.Pen()
+josh = turtle.Pen()
+tony = turtle.Pen()
 #drawing with the turtles
-avery.forward(50)
-avery.right(90)
-avery.forward(20)
-kate.left(90)
+#challenge was to draw pitchfork
+avery.forward(100)
 kate.forward(100)
+josh.forward(110)
+tony.forward(110)
+avery.left(90)
+kate.right(90)
+josh.left(90)
+tony.right(90)
+avery.forward(60)
+kate.forward(60)
+josh.forward(30)
+tony.forward(30)
+avery.right(90)
+kate.left(90)
+josh.right(90)
+tony.left(90)
+avery.forward(40)
+kate.forward(40)
+josh.forward(20)
+tony.forward(20)
 #keeps canvas window open
 turtle.done()
 
